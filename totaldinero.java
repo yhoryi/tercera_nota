9140085a53e87d92f12295e7096ef972cb049d4f
yhoryi <yhoryicarrascal@gmail.com>
2024-11-29 15:44:28 -0500

    Update totaldinero.java

--- totaldinero.java
+++ totaldinero.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        //Jhon Hamilton Gómez Molina 192436
      int saldoInicial=1000;
      int cantidadRetiradaPorSemana=200;
      int semanas=4;
